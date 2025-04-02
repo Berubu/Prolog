d2788a4d3df1e2725bb25b861b9611d2701fa86a
BERU <93852000+Berubu@users.noreply.github.com>
2025-04-02 11:57:20 -0600

    Actividad

---
+++ Prolog/Actividad.pl
@@ -0,0 +1,37 @@
+##Problema 1: Los guardianes de los templos
+##Cuatro guardianes (Apolo, Hécate, Ares y Hermes) custodian templos mitológicos asociados con diferentes elementos (fuego, agua, tierra y aire). Sabemos que:
+##Apolo no cuida el templo de fuego ni el de tierra.
+##Hécate no cuida el templo de aire.
+##Ares no cuida el templo de agua ni de aire.
+##Hermes cuida el templo de fuego o de agua.
+
+##Pregunta: ¿Qué templo cuida cada guardián?
+
+
+##el o se pone ; 
+
+
+guardianes([apolo, hecate, ares, hermes]).
+templos([fuego, agua, tierra, aire]).
+
+asignar_templos(asignacion) :- asignacion = [apolo-TA, hecate-TH, ares-TAr, hermes-THm], templos(T), permutation(T, [TA, TH, TAr, THm]),
+    TA \= fuego, TA \= tierra,  % apolo no cuida fuego ni tierr
+    TH \= aire,                 % Hécate no cuida aire
+    TAr \= agua, TAr \= aire,   % Ares no cuida agua ni aire
+    (THm = fuego ; THm = agua). % Hermes cuida fuego o agua
+
+?- asignar_templos(Asignacion).
+
+# 2
+
+heroes([aquiles, perseo, hercules, teseo]).
+armas([espada, lanza, arco, escudo]).
+
+% Regla para asignar armas
+asignar_armas(asignacion) :- asignacion = [aquiles-AA, perseo-AP, hercules-AH, teseo-AT], armas(A),
+    permutation(A, [AA, AP, AH, AT]), AA \= escudo, AA \= arco,  AP \= espada, AH \= lanza, AH \= escudo, (AT = arco ; AT = escudo).  
+
+?- asignar_armas(Asignacion).
+
+
+
--- Prolog/Factorial.pl
+++ Prolog/Factorial.pl
@@ -8,3 +8,12 @@ fibonacci(N, F) :-
     fibonacci(N2, F2), 
     F is F1 + F2.
 
+fibonacci(4, f):- 4 > 1, N1 is 3 +, N2 is 2, fibonacci(3, F1), fibonacci(2, F2), F is F1 + F2.
+fibonacci(2, f2):- 2 > 1, N1 is 1 +, N2 is 0, fibonacci(1, 1), fibonacci(0, 0), F2 is 1 + 0.
+fibonacci(3, f1):- 3 > 1, N1 is 2 +, N2 is 1, fibonacci(2, F3), fibonacci(1, F1), F1 is F3 + 1.
+fibonacci(2, f3):- 2 > 1, N1 is 1 +, N2 is 0, fibonacci(1, F1), fibonacci(0, F2), F3 is 1 + 0.
+
+
+
+a travez de la unificacion darle respuesta 
+cual es el proceso de la unificacion en prolog
